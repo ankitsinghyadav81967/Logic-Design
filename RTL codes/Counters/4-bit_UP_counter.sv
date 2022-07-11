ee0c0e6f360d89dcd9ae0cf8bd332fdff2079073
Saransh Choudhary <scsc2699@gmail.com>
2022-07-12 02:19:24 +0530

    Update 4-bit_UP_counter.sv
    Code for a 4-bit UP counter. Observe that the signal out is hardcoded to 4-bit length. In other words, this is a MOD-16 counter, i.e., the counter resets after counting 16 times (starting at 0 and terminating at 15).

--- RTL codes/Counters/4-bit_UP_counter.sv
+++ RTL codes/Counters/4-bit_UP_counter.sv
@@ -1,1 +1,20 @@
+`default_nettype none
+module UP_counter      (
+                           input logic clk,
+                           input logic rst,
+                           output logic [3:0] out  
+                       );
 
+logic [3:0] out_int ;                        
+
+always_ff @ (posedge clk or negedge rst) begin
+      if (~rst)   
+         out_int <= 'b0;
+      else begin
+         out_int <= out_int + 1;
+      end
+end
+
+assign out = out_int ; 
+
+endmodule 
