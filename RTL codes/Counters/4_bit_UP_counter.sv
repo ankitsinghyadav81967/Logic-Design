3a08abc3a934f5f5f3230edfc8da46f29c13755d
Saransh Choudhary <scsc2699@gmail.com>
2022-07-12 23:17:42 +0530

    Update 4_bit_UP_counter.sv
    Removed internal wire

--- RTL codes/Counters/4_bit_UP_counter.sv
+++ RTL codes/Counters/4_bit_UP_counter.sv
@@ -4,18 +4,15 @@ module Four_bit_UP_counter(
                                   input logic clk,
                                   input logic rst,
                                   output logic [3:0] out  
-                          );
-
-logic [3:0] out_int ;                        
+                          );                    
 
   always_ff @ (posedge clk or negedge rst) begin  // few compilers like Icarus don't understand always_ff statement, so use always in such cases
       if (~rst)   
-         out_int <= 'b0;
+         out <= 'b0;
       else begin
-         out_int <= out_int + 1;
+         out <= out + 1;
       end
 end
 
-assign out = out_int ; 
 
 endmodule 
