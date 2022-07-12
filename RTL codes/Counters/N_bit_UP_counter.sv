a16d2f30227dfe520ef70d1eed7fb92e642ed838
Saransh Choudhary <scsc2699@gmail.com>
2022-07-13 00:33:49 +0530

    Update N_bit_UP_counter.sv

--- RTL codes/Counters/N_bit_UP_counter.sv
+++ RTL codes/Counters/N_bit_UP_counter.sv
@@ -1,9 +1,9 @@
 `timescale 1ns/1ps
 `default_nettype none
-module N_bit_UP_counter  # (parameter MOD_VALUE = 3)    (                                           // this param is provided in design/TB for programmability
+module N_bit_UP_counter  # (parameter LENGTH = 3)    (                                           // this param is provided in design/TB for programmability
                                                             input logic clk,
                                                             input logic rst,
-                              	                            output logic [MOD_VALUE-1:0] out  
+                                                            output logic [LENGTH-1:0] out  
                           	                            );
                         
 always @ (posedge clk or negedge rst) begin
