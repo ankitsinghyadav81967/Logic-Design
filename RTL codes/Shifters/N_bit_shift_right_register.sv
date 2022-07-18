3d39b67e1a1260a8240374a127d7f81b5be6ef75
Saransh Choudhary <scsc2699@gmail.com>
2022-07-18 13:27:37 +0530

    Create N_bit_shift_right_register.sv

---
+++ RTL codes/Shifters/N_bit_shift_right_register.sv
@@ -0,0 +1,22 @@
+`timescale 1ns/1ps
+module shift_right_register #(parameter MSB = 4)
+  							(
+  								input logic clk,
+  								input logic rst,
+  								input logic d,
+  								input logic en,
+                  output logic [MSB-1:0] out
+					  		);	
+  
+always @ (posedge clk or posedge rst) begin
+    if (rst)
+      		out <= 4'b1011;
+  	else begin
+              if (~en)
+        	            out <= out;
+              else
+                      out <= {d,out[MSB-1:1]};
+    end
+end
+  
+endmodule
